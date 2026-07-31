3fb23fa153a938c0ae27fc86c8b63252ef774dc4
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2026-07-31 14:04:10 +0200

    feat: add publish newsletter feature

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -50,7 +50,7 @@ impl ResponseError for SubscribeError {
     }
 }
 
-fn error_chain_fmt(
+pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
@@ -148,7 +148,7 @@ pub async fn send_confirmation_email(
 
     email_client
         .send_email(
-            new_subscriber.email,
+            &new_subscriber.email,
             "Welcome",
             &format!(
                 "Welcome to our newsletter!<br/> Click <a href=\"{}\">here</a>",
