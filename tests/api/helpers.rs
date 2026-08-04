2e534903fb78a7634d4bd27f8fd7869e77b92205
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2026-08-04 13:07:37 +0200

    feat: use salt hash password

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,3 +1,7 @@
+use argon2::{
+    Argon2, PasswordHasher,
+    password_hash::{self, SaltString},
+};
 use reqwest::Url;
 use secrecy::Secret;
 use sha3::Digest;
@@ -160,8 +164,12 @@ impl TestUser {
     }
 
     async fn store(&self, pool: &PgPool) {
-        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
-        let password_hash = format!("{:x}", password_hash);
+        let salt = SaltString::generate(&mut rand::thread_rng());
+        let password_hash = Argon2::default()
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
+
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
             VALUES ($1, $2, $3)",
