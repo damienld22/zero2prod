dcab7cd95ae1ce5c45eb1dddb718a7bc7683a7bb
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2026-08-18 08:03:41 +0200

    feat: use typed session

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,4 +1,3 @@
-use actix_session::Session;
 use actix_web::{HttpResponse, error::InternalError, http::header::LOCATION, web};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::Secret;
@@ -7,6 +6,7 @@ use sqlx::PgPool;
 use crate::{
     authentication::{AuthError, Credentials, validate_credentials},
     routes::error_chain_fmt,
+    session_state::TypedSession,
 };
 
 #[derive(serde::Deserialize)]
@@ -22,7 +22,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -35,8 +35,9 @@ pub async fn login(
         Ok(user_id) => {
             tracing::Span::current().record("user_id", tracing::field::display(&user_id));
 
+            session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
 
             Ok(HttpResponse::SeeOther()
